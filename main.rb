eb2dec0ecaa824868ca1a99a86efedfe06460c3f
Valentin <levshits.l@gmail.com>
2014-09-15 11:38:50 +0300

    first step was made, but with logical error

--- main.rb
+++ main.rb
@@ -1,48 +1,15 @@
 require 'tk'
+require 'tkextlib/tile'
 require_relative 'experiment_element'
 
-
-def enter_button_click(elements_count, heads_count)
-  p elements_count
-  p heads_count
+def mousebutton_click(canvas, x_max, y_max)
+  canvas.delete('all')
+  experiment = Experiment.new(2000,x_max,y_max)
+  experiment.k_middle(10,canvas, x_max,y_max)
 end
-main_window = TkRoot.new { title "Methodology of solutions accepting"
-  minsize(600,500)
-  padx 10
-  pady 10
-}
-dataframe = TkFrame.new {
-  pack('side' => 'top')
-  padx 10
-  pady 10
-  width main_window.width
-}
 
-canvasframe = TkFrame.new {
-  pack('side' => 'top')
-  padx 10
-  pady 10
-  width main_window.width
-  background 'green'
-}
-
-TkLabel.new(dataframe){text "Elements count"; pack('side'=>'left')}
-elements_count = TkSpinbox.new(dataframe) do
-  to 5000
-  from 100
-  increment 100
-  state 'readonly'
-  pack 'side'=>'left'
-end
-TkLabel.new(dataframe){text "Heads count"; pack('side'=>'left')}
-heads_count = TkSpinbox.new(dataframe) do
-  to 20
-  from 1
-  increment 1
-  state 'readonly'
-  pack 'side'=>'left'
-end
-  canvas = TkCanvas.new(canvasframe) {background 'red';  place('height' => 500, 'width' => 50); grid}
-  TkcLine.new( canvas, 0,0,500,1500)
-enter_button = TkButton.new(dataframe){text "Enter"; pack; command proc{enter_button_click elements_count.get, heads_count.get}}
+main_window = TkRoot.new {title  "Methodology of solutions accepting"; minsize(500,400)}
+canvas = TkCanvas.new(main_window) {background 'white'}
+canvas.pack('fill'=>'both', 'expand'=>'yes')
+canvas.bind( "1", proc{mousebutton_click(canvas, TkWinfo.width(canvas), TkWinfo.height(canvas))})
 TkRoot.mainloop
