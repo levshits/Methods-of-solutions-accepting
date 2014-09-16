efbfc94b6fcbccaaace140f361951c5e70648a39
Valentin <levshits.l@gmail.com>
2014-09-16 11:48:17 +0300

    Added UI

--- experiment_element.rb
+++ experiment_element.rb
@@ -1,16 +1,16 @@
 require 'tk'
 class Experiment_element
-  attr_accessor :x,:y
+  attr_accessor 'x', 'y'
   def initialize(x_max, y_max)
     @x = rand x_max
     @y = rand y_max
   end
   def get_distance(element)
-    return Math.sqrt((@x-element.x)**2+(@y-element.y)**2)
+    Math.sqrt((@x-element.x)**2+(@y-element.y)**2)
   end
 end
 class Cluster < Experiment_element
-  attr_accessor :x, :y, :index,:cluster_members
+  attr_accessor 'x', 'y', 'index', 'cluster_members'
   def initialize(element, index)
     @x = element.x
     @y = element.y
@@ -40,7 +40,7 @@ class Cluster < Experiment_element
       @y = @cluster_members[@posibly_index].y
       return false
     end
-    return true
+    true
   end
   def add_member(element, index)
     @cluster_members.push element
@@ -52,8 +52,19 @@ class Cluster < Experiment_element
                @cluster_members[index].x+4,@cluster_members[index].y+4,'fill'=>color)}
     TkcOval.new(canvas,@x-4,@y-4,@x+4,@y+4, 'fill'=>'red')
   end
+  def find_the_farthest_element
+    max_distance = 0
+    max_element_index = 0
+    (0...@cluster_members.size).each{|index|
+      temp = self.get_distance(cluster_members[index])
+      if temp > max_distance
+        max_distance = temp
+        max_element_index = @cluster_members_index[index]
+      end}
+    [max_distance,max_element_index]
+  end
   def clear
-    if !@cluster_members.empty?
+    unless @cluster_members.empty?
       @cluster_members.clear
       @cluster_members_index.clear
     end
@@ -68,28 +79,76 @@ class Experiment
     @x_max = x_max
     @y_max = y_max
   end
+  def draw_elements(canvas)
+    color_list = %w(red blue green yellow cyan black azure purple deeppink bisque)
+    (0...@clusters.size).each{|i| @clusters[i].draw_elements(canvas, color_list[i])}
+  end
+  def dispetch_elements(canvas)
+    is_ready = false
+    until is_ready
+      (0...@clusters.size).each { |i| @clusters[i].clear }
+      canvas.delete('all')
+      self.sort_elements
+      self.draw_elements(canvas)
+      is_ready = true
+      (0...@clusters.size).each { |i|
+        is_ready = @clusters[i].is_central_element? && is_ready }
+    end
+  end
   def randomly_choose_clusters(clusters_count)
     @clusters = Array.new()
     index_array = Array.new
-    (0...clusters_count).each{ |i|
+    (0...clusters_count).each{ |_|
       index = rand @list_elements.size
-      while(index_array.include? index)
+      while index_array.include? index
         index = rand @list_elements.size
       end
       index_array.push(index)
       @clusters.push Cluster.new(@list_elements[index], index)
     }
   end
-  def draw_elements(canvas)
-    color_list = ['red','blue','green','yellow','cyan','black','azure','purple','deeppink','bisque']
-    (0...@clusters.size).each{|i| @clusters[i].draw_elements(canvas, color_list[i])}
+  def choose_clusters_by_maximin(canvas)
+    @clusters = Array.new
+    index = rand @list_elements.size
+    @clusters.push Cluster.new(@list_elements[index], index)
+    is_search_end = false
+    until is_search_end
+      self.sort_elements
+      max_elements = [0, 0]
+      (0...@clusters.size).each { |i| temp = @clusters[i].find_the_farthest_element
+      if temp[0]> max_elements[0]
+        max_elements[1] = temp[1]
+        max_elements[0] = temp[0]
+      end }
+      sum_distance = 0
+      if @clusters.size>1
+        (0...@clusters.size).each { |i| sum_distance += @clusters[i].get_distance(@clusters[i-1]) }
+      end
+      is_search_end = true
+      sum_distance/=@clusters.size
+      p @clusters.size
+      if @clusters.size>1
+        sum_distance/=2
+      end
+      p sum_distance
+      p max_elements
+      if sum_distance < max_elements[0]
+        @clusters.push Cluster.new(@list_elements[max_elements[1]], max_elements[1])
+        (0...@clusters.size).each { |i| @clusters[i].clear }
+        is_search_end = false
+      end
+    end
+    self.draw_elements(canvas)
+    Tk.update_idletasks()
+    sleep(3)
+    self.dispetch_elements(canvas)
   end
+
   def sort_elements
     max_distance = Math.sqrt(@x_max**2+@y_max**2)
     (0...@list_elements.size).each{|index| distance = max_distance
-    min_distance_index = -1
+    min_distance_index = 0
     (0...@clusters.size).each{|head_index| temp = @clusters[head_index].get_distance(@list_elements[index])
-    p temp
     if temp < distance
       distance = temp
       min_distance_index =head_index
@@ -98,15 +157,7 @@ class Experiment
   end
   def k_middle(clusters_count,canvas)
     self.randomly_choose_clusters(clusters_count)
-    is_ready = false
-    while(!is_ready)
-      (0...@clusters.size).each{|i| @clusters[i].clear}
-      canvas.delete('all')
-      self.sort_elements
-      self.draw_elements(canvas)
-      is_ready = true
-      (0...@clusters.size).each{|i|
-        is_ready = @clusters[i].is_central_element? && is_ready}
-    end
+    self.dispetch_elements(canvas)
   end
+
 end
--- main.rb
+++ main.rb
@@ -2,14 +2,36 @@ require 'tk'
 require 'tkextlib/tile'
 require_relative 'experiment_element'
 
-def mousebutton_click(canvas, x_max, y_max)
+
+def mousebutton_click(canvas, x_max, y_max )
   canvas.delete('all')
-  experiment = Experiment.new(100,x_max,y_max)
-  experiment.k_middle(3,canvas)
+  p "Please choose algoritm"
+  p "1. Algorithm k-means."
+  p "2. Algorithm maximina."
+  p "0. Exit."
+  case gets.chomp.to_i
+    when 0
+      Tk.exit
+    when 2
+      p "Please enter count of elements"
+      elements_count = gets.chomp.to_i
+      experiment = Experiment.new(elements_count,x_max,y_max)
+      experiment.choose_clusters_by_maximin(canvas)
+    when 1
+      p "Please enter count of elements"
+      elements_count = gets.chomp.to_i
+      p "Please enter count of classes"
+      classes_count = gets.chomp.to_i
+      experiment = Experiment.new(elements_count,x_max,y_max)
+      experiment.k_middle(classes_count,canvas)
+  end
+  p "ready"
 end
 
 main_window = TkRoot.new {title  "Methodology of solutions accepting"; minsize(500,400)}
 canvas = TkCanvas.new(main_window) {background 'white'}
 canvas.pack('fill'=>'both', 'expand'=>'yes')
 canvas.bind( "1", proc{mousebutton_click(canvas, TkWinfo.width(canvas), TkWinfo.height(canvas))})
+
+
 TkRoot.mainloop
